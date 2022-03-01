275847a767b165dcb6e32f73f1f22d54eb727f6f
jrkozak92 <jrkozak92@gmail.com>
2022-03-01 14:59:35 -0600

    dotenv and Atlas connection

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require('express')
 const app = express()
+require('dotenv').config()
 
-app.listen(3000, () => {
+const PORT = process.env.PORT || 3003;
+const MONGODB_URI = process.env.MONGODB_URI;
+
+mongoose.connect(MONGODB_URI);
+db.on('connected', () => console.log('mongo connected: ', MONGODB_URI));
+
+app.listen(PORT, () => {
   console.log("listening on 3000...");
 })
