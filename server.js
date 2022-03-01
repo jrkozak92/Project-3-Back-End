f14cc8660c199e866e26f5789a518e651dccce94
jrkozak92 <jrkozak92@gmail.com>
2022-03-01 15:24:55 -0600

    Testing Heroku/Mongo stuff

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
+const db = mongoose.connection
 require('dotenv').config()
 
 const PORT = process.env.PORT || 3003;
 const MONGODB_URI = process.env.MONGODB_URI;
 
 mongoose.connect(MONGODB_URI);
-db.on('connected', () => console.log('mongo connected: ', MONGODB_URI));
+db.on('connected', () => console.log('mongo connected: ', PROJECT3_DB));
 
 app.listen(PORT, () => {
   console.log("listening on 3000...");
+})
+
+app.get('/', () => {
+  res.send('Hello')
 })
