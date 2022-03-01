48703e2b3053fa955043fff20d021cdff27d17b7
Nathan Freeman <NathanLFreeman@gmail.com>
2022-03-01 16:14:44 -0500

    update package.json with engines/node version, test mongodb connection in server.js

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
+const db = mongoose.connection
 require('dotenv').config()
 
 const PORT = process.env.PORT || 3003;
