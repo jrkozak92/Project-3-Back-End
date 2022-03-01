46a67c7205c375a507eb4dd09ad19fa9c03eb5aa
jrkozak92 <jrkozak92@gmail.com>
2022-03-01 15:35:43 -0600

    chaning connection string variable

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3003;
 const MONGODB_URI = process.env.MONGODB_URI;
 
 mongoose.connect(MONGODB_URI);
-db.on('connected', () => console.log('mongo connected: ', PROJECT3_DB));
+db.on('connected', () => console.log('mongo connected: ', MONGODB_URI));
 
 app.listen(PORT, () => {
   console.log("listening on 3000...");
