c6ef4be36af90024fe212a907d403a59b650d38d
jrkozak92 <jrkozak92@gmail.com>
2022-03-03 11:44:48 -0600

    User Auth, accouont creation done, working on session

---
+++ controllers/sessions.js
@@ -0,0 +1,4 @@
+const bcrypt = require('bcrypt')
+const express = require('express')
+const sessions = express.Router()
+const User = require('../models/users.js')
--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const cors = require('cors')
 require('dotenv').config()
 const charactersController = require('./controllers/characters.js')
 const episodesController = require('./controllers/episodes.js')
+const usersController = require('./controllers/users.js')
+const session = require('express-session')
+const sessionsController = require('./controllers/sessions.js')
 
 const PORT = process.env.PORT || 3003;
 const MONGODB_URI = process.env.MONGODB_URI;
@@ -16,10 +19,19 @@ db.on('connected', () => console.log('mongo connected: ', MONGODB_URI));
 //Middleware
 app.use(express.json())
 app.use(cors())
+app.use(
+  session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: false
+  })
+)
 
 //controllers/routing
 app.use('/characters', charactersController)
 app.use('/episodes', episodesController)
+app.use('/users', usersController)
+app.use('/sessions', sessionsController)
 
 app.get('/', (req, res) => {
   res.redirect('/characters')
